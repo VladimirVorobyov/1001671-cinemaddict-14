ca2f5d4a3c5a0a17c9c654180174e739f0ef7616
Vova Vorobiev <vovabandit1945@gmail.com>
2021-04-01 09:42:13 +0300

    lesson2

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import { createFilmsTemplate } from './view/films.js';
 import { createCardTemplate } from './view/card.js';
 import { createButtonTemplate } from './view/sow-more.js';
 import { createFilmsTopTemplate } from './view/top-rated.js';
-import { createMostCommentedTemplate } from './view/most-commented.js';
 const FILMS_NUMBER = 5;
 const FILMS_TOP = 2;
 
@@ -31,10 +30,7 @@ render(filmsList, createButtonTemplate(), 'beforeend');
 
 for(let i = 0; i < FILMS_TOP; i++){
   const topRated = document.querySelector('#top-rated');
+  const mostCommented = document.querySelector('#most-commented');
   render(topRated, createFilmsTopTemplate(), 'beforeend');
-}
-
-for(let i = 0; i < FILMS_TOP; i++){
-  const topRated = document.querySelector('#most-commented');
-  render(topRated, createMostCommentedTemplate(), 'beforeend');
+  render(mostCommented, createFilmsTopTemplate(), 'beforeend');
 }
