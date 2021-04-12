d698fa6c6ac27e9acfa4a6feaf8aeba68999e6f3
Vova Vorobiev <vovabandit1945@gmail.com>
2021-04-12 08:13:53 +0300

    lesson1

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import {render, RenderPosition} from './util.js';
+import { generateMovie } from './mock/movie.js';
+import { createFilter } from './mock/filter.js';
 import MenuTemplateView  from './view/main-navigation.js';
 import SortTemplateView  from './view/main-sort.js';
 import  UserTemplateView  from './view/user.js';
@@ -7,13 +9,11 @@ import CardView from './view/card.js';
 import ButtonTemplateView  from './view/sow-more.js';
 import PopupTemplateView  from './view/popup.js';
 import NumberMoviesView  from './view/number-fims.js';
-import { generateMovie } from './mock/movie.js';
 import comments from './mock/comment.js';
 import CommentView from './view/comment.js';
 import GenerePopupView from './view/genere.js';
 import FilterNavigationView from './view/filter.js';
 import NoFilmsTemplateView from './view/no-films.js';
-import { createFilter } from './mock/filter.js';
 
 const FILMS_NUMBER = 15;
 const FILMS_TOP = 2;
